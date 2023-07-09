8f626f9e46455b0ab6b475d381f4faba4153d572
Vinson Huang <vinsonh789@gmail.com>
2023-07-08 22:30:16 -0500

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <Arduino.h>
+
+#define YELLOWPIN 6
+#define BLUEPIN 2
+
+volatile long degrees;
+
+void CountDegrees() {
+  if (digitalRead(YELLOWPIN) == digitalRead(BLUEPIN)) {
+    degrees++;
+  } else {
+    degrees--;
+  }
+}
+
+void setup() {
+  Serial.begin(9600);
+  while (!Serial);
+
+  pinMode(YELLOWPIN, INPUT);
+  pinMode(BLUEPIN, INPUT);
+
+  degrees = 0;
+
+  attachInterrupt(digitalPinToInterrupt(BLUEPIN), CountDegrees, CHANGE);
+}
+
+void loop() {
+  static long deg = 0;
+
+  if (degrees != deg) {
+    Serial.println(degrees);
+    deg = degrees;
+  }
+}
